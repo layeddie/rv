319d49488398bef13a662381518046a53076d06e
elay12 <layeddie@gmail.com>
2021-03-30 18:38:07 +0100

    added rv_app_firmware with mix nerves.new rv_app_firmware

---
+++ rv_app_firmware/lib/rv_app_firmware.ex
@@ -0,0 +1,18 @@
+defmodule RvAppFirmware do
+  @moduledoc """
+  Documentation for RvAppFirmware.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RvAppFirmware.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ rv_app_firmware/test/rv_app_firmware_test.exs
@@ -0,0 +1,8 @@
+defmodule RvAppFirmwareTest do
+  use ExUnit.Case
+  doctest RvAppFirmware
+
+  test "greets the world" do
+    assert RvAppFirmware.hello() == :world
+  end
+end
